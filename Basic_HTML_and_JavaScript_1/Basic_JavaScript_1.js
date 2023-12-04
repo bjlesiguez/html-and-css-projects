2f33d75ecdaceb84c2a864800cfcadf05c1dbed8
bjlesiguez <136043137+bjlesiguez@users.noreply.github.com>
2023-12-03 19:01:40 -0700

    Assignment-Basic_html_css_javascript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+
+alert("Hello, World!");
+
+function My_First_Function() {
+	var String = "Kiss me, I'm B!";
+	var result = String.fontcolor("green");
+	document.getElementById("B") .innerhtml = result;
+	}
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Ben!";
+	var result = String.fontcolor("green");
+	document.getElementById("Ben") .innerHTML = result;
+	}
